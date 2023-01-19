969ff79733482f37d3781bf460f074f2b4bfad2e
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-19 22:51:07 +0200

    added 5.7

--- 5.6_mask.js
+++ 5.6_mask.js
@@ -3,5 +3,5 @@ const maskify = (str) =>
     ? "#".repeat(str.length - 4) + str.split("").slice(-4).join("")
     : str;
 
-console.log(maskify("23"));
-console.log(maskify("afarsemon"));
+// console.log(maskify("23"));
+// console.log(maskify("afarsemon"));
