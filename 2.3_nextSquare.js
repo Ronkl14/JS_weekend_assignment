8569141ab730e81b2532d6115ecce0a1e22ce62f
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-19 15:15:38 +0200

    added another way

--- 2.3_nextSquare.js
+++ 2.3_nextSquare.js
@@ -10,8 +10,18 @@ const nextSquare = (num) => {
   }
 };
 
+// another way
+const nextSquare2 = (num) =>
+  Number.isInteger(Math.sqrt(num)) ? (Math.sqrt(num) + 1) ** 2 : -1;
+
+//checks
 // console.log(nextSquare(69));
 // console.log(nextSquare(121));
 // console.log(nextSquare(144));
 // console.log(nextSquare(13));
 // console.log(nextSquare(625));
+// console.log(nextSquare2(69));
+// console.log(nextSquare2(121));
+// console.log(nextSquare2(144));
+// console.log(nextSquare2(13));
+// console.log(nextSquare2(625));
