e78af7d33732ce68990c93240e1d7de106efe88f
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-19 15:55:17 +0200

    added 2.6

---
+++ 2.6_century.js
@@ -0,0 +1,7 @@
+const getCentury = (year) => Math.ceil(year / 100);
+
+// console.log(getCentury(1705));
+// console.log(getCentury(2000));
+// console.log(getCentury(1601));
+// console.log(getCentury(101));
+// console.log(getCentury(1900));
