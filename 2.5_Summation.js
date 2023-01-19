edc9212fb28c31ae96d8e205b92990a7c839feb7
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-19 15:37:17 +0200

    added 2.5

---
+++ 2.5_Summation.js
@@ -0,0 +1,10 @@
+const summation = (num) => {
+    let sum = 0;
+    for (let i = 1 ; i <= num ; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+// console.log(summation(8));
+// console.log(summation(10));
