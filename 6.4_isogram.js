218d0979e4a1ada53e31c7120bac8e616cb2ae1c
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-22 01:38:59 +0200

    updated 6.4

--- 6.4_isogram.js
+++ 6.4_isogram.js
@@ -14,5 +14,16 @@ const isIsogram = (str) => {
   return counter === 0 ? true : false;
 };
 
+const isIsogram2 = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // console.log(isIsogram("aba"));
 // console.log(isIsogram("abc"));
+// console.log(isIsogram2("aba"));
+// console.log(isIsogram2("abc"));
--- 7_implement.js
+++ 7_implement.js
@@ -28,3 +28,10 @@ const mapAlt = (arr, callbackFn) => {
 };
 
 console.log(mapAlt([1, 2, 3, 4, 5, 6], (x) => x * 3));
+
+let arr1 = [1, 2, 3, 4, 5];
+forEachAlt(arr1, (x) => x * 3);
+console.log(arr1);
+let arr2 = [1, 2, 3, 4, 5];
+mapAlt(arr2, (x) => x * 3);
+console.log(arr2);
